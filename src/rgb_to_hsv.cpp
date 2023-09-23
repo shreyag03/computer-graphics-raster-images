3dab737906b515e831edf9970d9350d1a5ac1884
shreyag03 <shreyag.tech@gmail.com>
2023-09-22 22:50:37 -0400

    modified rgb to hsv

--- src/rgb_to_hsv.cpp
+++ src/rgb_to_hsv.cpp
@@ -1,4 +1,6 @@
 #include "rgb_to_hsv.h"
+#include <vector>
+#include <algorithm>
 
 void rgb_to_hsv(
   const double r,
@@ -10,6 +12,14 @@ void rgb_to_hsv(
 {
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here:
+  
+  //   r  red intensity [0,1]
+  double red = std::max(0.0, std::min(r, 1.0));
+  //   g  green intensity [0,1]
+  double green = std::max(0.0, std::min(g, 1.0));
+  //   b  blue intensity [0,1]
+  double blue = std::max(0.0, std::min(b, 1.0));
+  
   h = 0;
   s = 0;
   v = 0;
